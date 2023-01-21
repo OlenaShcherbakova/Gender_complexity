524e66543a28f3c9e2742794f9d9dd6dbd051bdf
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2023-01-21 18:22:38 +0100

    BT files

---
+++ world_minitest/features_Semantic_classes_Agreement_patterns.dependent.cmd
@@ -0,0 +1,9 @@
+4
+2
+ScaleTrees
+Burnin 1000
+Iterations 10000
+Stones 100 1000
+LogFile features_Semantic_classes_Agreement_patterns.dependent.log
+Run
+
---
+++ world_minitest/features_Semantic_classes_Agreement_patterns.independent.cmd
@@ -0,0 +1,10 @@
+4
+2
+TestCorrel
+ScaleTrees
+Burnin 1000
+Iterations 10000
+Stones 100 1000
+LogFile features_Semantic_classes_Agreement_patterns.independent.log
+Run
+
