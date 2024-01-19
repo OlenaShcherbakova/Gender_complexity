1432764c0f044e986f5b08a4ac9ef6240653b7eb
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2024-01-19 13:16:26 +0100

    Update phylopath_summary_results.R

--- phylopath_summary_results.R
+++ phylopath_summary_results.R
@@ -9,8 +9,30 @@ all <- as.data.frame(rbind(ie, b, w)) %>%
 
 colnames(all) <- gsub("\\.", " ", colnames(all))
 
+ie_b_latex <- all[1:38,] %>%
+  dplyr::select(-1)
+colnames(ie_b_latex) <- gsub("\\.", " ", colnames(ie_b_latex))
+
+world_latex <- all[39:57,] %>%
+  dplyr::select(-1)
+colnames(world_latex) <- gsub("\\.", " ", colnames(world_latex))
 
 all_latex <- all %>%
+  kbl(caption="Phylogenetic path analysis on three phylogenies",
+      format="latex") %>% #,
+  kable_minimal(full_width = F) %>%
+  kable_styling(latex_options = c("scale_down")) %>% #font_size = 4 for saving space
+  column_spec(1, width = "22em")
+
+#tables generated including numbered first row
+ie_b_latex <- ie_b_latex %>%
+  kbl(caption="Phylogenetic path analysis on three phylogenies",
+      format="latex") %>% #,
+  kable_minimal(full_width = F) %>%
+  kable_styling(latex_options = c("scale_down")) %>% #font_size = 4 for saving space
+  column_spec(1, width = "22em")
+
+world_latex <- world_latex %>%
   kbl(caption="Phylogenetic path analysis on three phylogenies",
       format="latex") %>% #,
   kable_minimal(full_width = F) %>%
